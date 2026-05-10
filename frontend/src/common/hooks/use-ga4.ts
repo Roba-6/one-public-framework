3132da9b9f7b1fb8bd4de6f509d08a85e47e7ba9
Roba <roba@one-coder.com>
2026-05-10 23:25:02 +0900

    refactor: remove redundant `.ts` extensions from imports in various files

--- frontend/src/common/hooks/use-ga4.ts
+++ frontend/src/common/hooks/use-ga4.ts
@@ -2,8 +2,8 @@ import { useEffect } from 'react'
 import ReactGA from 'react-ga4'
 import { useLocation } from 'react-router'
 
-import { selectAppSettings, type Setting } from '@/common/app-slice.ts'
-import { useAppSelector } from '@/common/hooks/use-store.ts'
+import { selectAppSettings, type Setting } from '@/common/app-slice'
+import { useAppSelector } from '@/common/hooks/use-store'
 
 export const useGoogleAnalytics4 = () => {
   const location = useLocation()
